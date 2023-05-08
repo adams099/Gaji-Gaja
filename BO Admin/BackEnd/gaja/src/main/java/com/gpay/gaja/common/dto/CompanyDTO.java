e746335d6214a7719da341db97a2b2ed87fbcb18
adams099 <adammusyafa04@gmail.com>
2023-05-08 14:45:25 +0700

    table User & Company

---
+++ BO Admin/BackEnd/gaja/src/main/java/com/gpay/gaja/common/dto/CompanyDTO.java
@@ -0,0 +1,16 @@
+package com.gpay.gaja.common.dto;
+
+import lombok.Data;
+
+@Data
+public class CompanyDTO {
+
+    private Long id;
+    private String comName;
+    private String comTaxNum;
+    private String address;
+    private String mailAddress;
+    private String postal;
+    private String adminName;
+    private String adminEmail;
+}
