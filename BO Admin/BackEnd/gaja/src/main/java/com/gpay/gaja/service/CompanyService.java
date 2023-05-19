ce00957b3e7d05c58a671228056f2614d4232ad8
adams099 <adammusyafa04@gmail.com>
2023-05-19 13:52:49 +0700

    backend length table company

--- BO Admin/BackEnd/gaja/src/main/java/com/gpay/gaja/service/CompanyService.java
+++ BO Admin/BackEnd/gaja/src/main/java/com/gpay/gaja/service/CompanyService.java
@@ -7,10 +7,12 @@ import com.gpay.gaja.common.dto.CompanyDTO;
 public interface CompanyService {
 
     CompanyDTO findById(Long id);
-    
+
     CompanyDTO save(CompanyDTO companyDTO);
-    
+
     List<CompanyDTO> getAll();
-    
+
     Long getStatusCount(int status);
+
+    Long getTableLength();
 }
