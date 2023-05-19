c1acaee909e19dc24b0b840803e4f79a3350b88d
nanangpratama99 <pratamananang05@gmail.com>
2023-05-19 13:26:37 +0700

    update

---
+++ BO Admin/FrontEnd/bo-admin/src/components/CompanyComp2.vue
@@ -0,0 +1,116 @@
+<template>
+    <section class="home">
+
+        <button class="btn btn-success mr-auto">Add Company</button>
+
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col" style="width: 70px;">ID</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="text-center">
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td style="width: 100px;">
+                        <div class="button-action d-flex flex-row">
+                            <button class="btn btn-primary">Detail</button>
+                            <button class="btn btn-primary ml-2 mr-2">Detail</button>
+                            <button class="btn btn-primary">Detail</button>
+                        </div>
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+    </section>
+</template>
+  
+<script>
+
+export default {
+    name: "CompanyS",
+
+    // DATA
+    data() {
+        return {
+            companyDatas: {
+                comName: null,
+                mailAddress: null,
+                comTaxNum: null,
+                address: null,
+                siup: null,
+                postal: null,
+                adminName: null,
+                adminEmail: null,
+                status: null,
+                createdBy: null,
+                createdTime: null,
+                apprBy: null,
+                updateTime: null
+            },
+
+            buatAkun: {
+                name: null,
+                email: null,
+                pass: null,
+                roleId: null,
+                statId: null,
+                companyId: null,
+                created: null,
+                createdBy: null,
+                phone: null,
+                apprBy: null,
+                update: null
+            },
+
+            showForm: false,
+        }
+    },
+
+    methods: {
+    },
+};
+</script>
+  
+<style scoped>
+table {
+    width: 80vw;
+    margin-left: 50px;
+    margin-top: 50px;
+}
+
+tbody {
+    background-color: white;
+}
+
+
+thead {
+    background-color: #695cfe;
+    color: white;
+    text-align: center;
+}
+
+form {
+    width: 60vw;
+    padding: 40px 40px;
+    border-radius: 20px;
+}
+
+.iseng {
+    margin-left: 100px;
+}
+
+.add-company {
+    background-color: #695cfe;
+    color: white;
+    width: 100%;
+    border-radius: 10px;
+    margin-top: 15px;
+}
+</style>
--- BO Admin/FrontEnd/bo-admin/src/pages/Company.vue
+++ BO Admin/FrontEnd/bo-admin/src/pages/Company.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
         <Sidebar></Sidebar>
-        <CompanyComp></CompanyComp>
+        <CompanyComp2></CompanyComp2>
     </div>
 </template>
 
 <script>
 import Sidebar from "../components/SideBar.vue"
-import CompanyComp from "../components/CompanyComp.vue"
+import CompanyComp2 from "../components/CompanyComp2.vue"
 
 export default {
     name: "CompanyS",
     components: {
         Sidebar,
-        CompanyComp
+        CompanyComp2
     },
 }
 
